d03a2f4fc99178ae5df172287c9b66c5cb1f1a41
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-04-11 06:19:42 -0700

    Add files via upload

---
+++ SessionGenerator/__init__.py
@@ -0,0 +1,50 @@
+# Generate Session In Your Telegram premium @Opleech
+# Copyright (c) 2023 WOODcraft
+import logging
+import pyromod
+
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+from pyrogram import Client
+from pyrogram.enums import ParseMode
+
+import config
+
+logging.basicConfig(
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("oldpyro").setLevel(logging.ERROR)
+logging.getLogger("telethon").setLevel(logging.ERROR)
+LOGGER = logging.getLogger(__name__)
+
+mongo = MongoCli(config.MONGO_DB_URI)
+db = mongo.SessionGenerator
+
+
+class Opleech(Client):
+    def __init__(self):
+        super().__init__(
+            name="Opleech",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            lang_code="en",
+            bot_token=config.BOT_TOKEN,
+            in_memory=True,
+            parse_mode=ParseMode.HTML,
+        )
+
+    async def start(self):
+        await super().start()
+        self.id = self.me.id
+        self.name = self.me.first_name + " " + (self.me.last_name or "")
+        self.username = self.me.username
+        self.mention = self.me.mention
+
+    async def stop(self):
+        await super().stop()
+
+
+Opleech = Opleech()
---
+++ SessionGenerator/__main__.py
@@ -0,0 +1,28 @@
+# Generate Session In Your Telegram premium @Opleech
+# Copyright (c) 2023 WOODcraft
+import asyncio
+import importlib
+
+from pyrogram import idle
+
+from SessionGenerator import LOGGER, Opleech
+from SessionGenerator.modules import ALL_MODULES
+
+
+async def Opleech_boot():
+    try:
+        await Opleech.start()
+    except Exception as ex:
+        LOGGER.error(ex)
+        quit(1)
+
+    for all_module in ALL_MODULES:
+        importlib.import_module("SessionGenerator.modules." + all_module)
+
+    LOGGER.info(f"@{Opleech.username} Started.")
+    await idle()
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(Opleech_boot())
+    LOGGER.info("Stopping String Gen Bot...")
