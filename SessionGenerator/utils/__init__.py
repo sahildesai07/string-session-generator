fa04a803a5d0b3577a0e6df975b062e0df274128
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-04-11 06:20:43 -0700

    Add files via upload

---
+++ SessionGenerator/utils/__init__.py
@@ -0,0 +1,4 @@
+# Generate Session In Your Telegram premium @Opleech
+# Copyright (c) 2023 WOODcraft
+from .inline import *
+from .users import *
---
+++ SessionGenerator/utils/inline.py
@@ -0,0 +1,32 @@
+# Generate Session In Your Telegram premium @Opleech
+# Copyright (c) 2023 WOODcraft
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from config import SUPPORT_CHAT
+
+
+keyboard = InlineKeyboardMarkup(
+    [
+        [InlineKeyboardButton(text="❈ 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 ❈", callback_data="gensession")],
+        [
+            InlineKeyboardButton(text="❈ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 ❈", url=SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text="❈ 𝐎𝐩𝐥𝐞𝐞𝐜𝐡 ❈", url="https://t.me/Opleech_WD"
+            ),
+        ],
+    ]
+)
+
+gen_key = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(text="❣️ 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐯1 ❣️", callback_data="pyrogram1"),
+            InlineKeyboardButton(text="🦋 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐯2 🦋", callback_data="pyrogram"),
+        ],
+        [InlineKeyboardButton(text="🌼 𝐓𝐞𝐥𝐞𝐭𝐡𝐨𝐧 🌼", callback_data="telethon")],
+    ]
+)
+
+retry_key = InlineKeyboardMarkup(
+    [[InlineKeyboardButton(text="⚡️ 𝐓𝐫𝐲 𝐀𝐠𝐚𝐢𝐧 ⚡️", callback_data="gensession")]]
+)
